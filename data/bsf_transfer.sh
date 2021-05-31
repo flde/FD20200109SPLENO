0834cde8edf07d90c3858208943dde4d02b4d68b
Florian Deckert <fdeckert@m002.hpc.cemm.at>
2021-05-31 11:59:26 +0200

    working update cluster

--- data/bsf_transfer.sh
+++ data/bsf_transfer.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-#SBATCH --partition=longq
-#SBATCH --qos=longq
+#SBATCH --partition=shortq
+#SBATCH --qos=shortq
 #SBATCH --job-name bsf_transfer
-#SBATCH -o %j.out
-#SBATCH -e %j.err
+#SBATCH -o %x.out
+#SBATCH -e %x.err
 
-cp /nobackup/lab_bsf/projects/BSA_0501_SK_RG_LEUKO /research/peer/fdeckert/FD20210520LEUKO/data/
+cp -r /nobackup/lab_bsf/projects/BSA_0355_SM01_10x_SPLENO /research/peer/fdeckert/FD20200109SPLENO/data/
